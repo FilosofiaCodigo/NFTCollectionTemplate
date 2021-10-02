f4657e10d0ff0edff93baa193c0b3f28727259f0
Ahmed Castro <ahmed.hn.43@gmail.com>
2021-10-02 16:36:00 -0600

    Update MyNFTCollection.sol

--- contracts/MyNFTCollection.sol
+++ contracts/MyNFTCollection.sol
@@ -1,6 +1,6 @@
 // contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
