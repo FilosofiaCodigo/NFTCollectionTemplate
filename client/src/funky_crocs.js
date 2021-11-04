7d85566bc0fa516b138751b09bd2374d5334b97a
turupawn <ahmed.hn.43@gmail.com>
2021-11-04 13:43:20 -0600

    fix test

--- client/src/funky_crocs.js
+++ client/src/funky_crocs.js
@@ -89,6 +89,9 @@ const getContract = async (web3) => {
 }
 
 async function connectWallet() {
+  document.getElementById("web3_message").textContent="Connect X";
+  web3 = await getWeb3()
+  document.getElementById("web3_message").textContent="Connect Y";
   getAccounts()
 }
 
@@ -96,7 +99,7 @@ async function loadAccount() {
   accounts = await web3.eth.getAccounts()
   balance = await contract.methods.balanceOf(accounts[0]).call()
   document.getElementById("web3_message").textContent="Connected"
-  document.getElementById("connect_button").style.display = "none"
+  //document.getElementById("connect_button").style.display = "none"
   document.getElementById("nft_balance").textContent="You have " + balance + " Crocs"
 }
 
@@ -280,7 +283,8 @@ const setAddresses = async () => {
     [
       "0x707e55a12557E89915D121932F83dEeEf09E5d70",
       "0x707e55a12557E89915D121932F83dEeEf09E5d70",
-      "0x707e55a12557E89915D121932F83dEeEf09E5d70"]
+      "0x707e55a12557E89915D121932F83dEeEf09E5d70"
+    ]
     )
     .send({ from: accounts[0], gas: 0, value: 0 })
     .on('transactionHash', function(hash){
@@ -309,9 +313,67 @@ const withdrawTeam = async () => {
 const editWhitelistReserved = async () => {
   const result = await contract.methods.editWhitelistReserved(
     [
-      "0x66e1aa2125b255B63f7198F17ca5AFCf5e842449"
+      "0xe1A308d193cf262090108B028840174Fb2e7E20a",
+      "0x46E41eaac194fB00f88B2D27765bF31cc2F1a707",
+      "0x9c3211d23b63E4D9784600D5770540268DdB6372",
+      "0x4800e1dc56Dd78d9b071afbd0e90B06862516132",
+      "0x6B47cB1065a81B45784776bb5F85456fd8431e31",
+      "0x617aF4A7D97FE1C83B2A383713B5FCCF0D75F39C",
+      "0x971257beA317043f6aA786F5b88d0142e524305e",
+      "0xb8296ADf724315572Fd0283bA13967F78e3D17D7",
+      "0x78C0Fa5FcFa30d5f222d65B469EB1695d7f0724d",
+      "0x564f3A7B17F8744316cE393BDF1e2535EA0B2A47",
+      "0x73670ba1814a1e06E31e78bA4a4bB77293cf37D6",
+      "0x93e00bb056Eb95Fa4929573F7EE0A3F1a9a5469e",
+      "0x75BAa4C13f45923d0D712b2a8Ff0330a2DCD0c96",
+      "0x78C9DDf6cc304D01d56eFEAf6e3489d5be5a2ded",
+      "0x6bc9922A4cf67651a2CA650e868219Cd619ed31D",
+      "0x548010d30282A928165Aa6D2BDAA12F59e77785B",
+      "0x3580b8b357619af739531cf1ddf767322b2deffc",
+      "0xE4D2737E03dDDf7B25f5e6A07d934B56fBbAE1C2",
+      "0x9b166014f671e713e67b19fe91a692326086814b",
+      "0x1660D013cDe152A73Fac3699e8745F5412A4E6b9",
+      "0xe4a3051f00ceb480fbf2f3917878bbbd64644bd6",
+      "0x37ae2f47dfbc57e3b4f4aeb53ff1687ccf3ca3a2",
+      "0x023Cd3EF787056CE757AE079aCb49255cf95C194",
+      "0x58955980b61b65bfa6a6738ce146e575274af34b",
+      "0x09A31e9eA6490991995d4EceC3C5748B993064fd",
+      "0xCCCd5C571Ab86590227123039218238d5B88D19c",
+      "0x730bF3B67090511A64ABA060FbD2F7903536321E",
+      "0x66e1aa2125b255B63f7198F17ca5AFCf5e842449",
+      "0x75E82F64916bb536F8e27f6457Ae5C4Cc21BD677",
+      "0x1a02764a8531039d31ca05aef09ecdaed5b76873"
     ],
     [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      2,
+      1,
+      1,
+      2,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      2,
+      2,
+      2,
       1
     ]
     )
